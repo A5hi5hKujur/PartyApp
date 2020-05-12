ee68ebafab161364970bf90cd26cf51ca2729e80
Ravi Kumar <57840416+ravi-poddar@users.noreply.github.com>
2020-05-12 22:24:30 +0530

    resolved global variable in app.js

--- app.js
+++ app.js
@@ -50,6 +50,15 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+  
+
 //=================
 //ROUTES
 //=================
