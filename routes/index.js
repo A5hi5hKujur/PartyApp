e83069eacd8e0c49f45bc265cbb84ce3c2c5b50a
Ravi Keshri <61163354+ravikeshri@users.noreply.github.com>
2020-05-13 13:57:26 +0530

    fixed dashboard
    Now dashboard is specific to logged in user

--- routes/index.js
+++ routes/index.js
@@ -14,28 +14,30 @@ router.get('/', forwardAuthenticated, (req, res) => res.render('landing'));
 
 // Dashboard
 router.get('/dashboard', isLoggedIn, (req, res) => {
-  Party.find({}, (err, parties) => {
+  var userParties = req.user.parties;
+  Party.find().where('_id').in(userParties).exec((err, parties) => {
     if (err) {
       console.log(err);
       res.redirect('/dashboard');
     } else {
-      res.render('index',
-      {
-        parties: parties,
-        user: req.user
-      });
+      res.render('index', {parties: parties});
     }
   });
 });
 
 // Dashboard sort-section route to handle asynchronous toggle
 router.get('/dashboard.json', isLoggedIn, (req, res) => {
-  Party.find({}, (err, parties) => {
+  var userParties = req.user.parties;
+  Party.find().where('_id').in(userParties).exec((err, parties) => {
     if (err) {
       console.log(err);
       res.redirect('/dashboard');
     } else {
-      res.json(parties);
+      if(req.xhr) {
+        res.json(parties);
+      } else {
+        res.render('index', {parties: parties});
+      }
     }
   });
 });
