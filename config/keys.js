2f24a0566680fb4ea6369351174566bbf579dddf
Ravi Kumar <57840416+ravi-poddar@users.noreply.github.com>
2020-05-17 23:18:21 +0530

    minor fix in key.js one double quote  was missing i added that

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
-const dbPassword = "mongodb+srv://ravi:ravikumar@cluster0-nwcfy.mongodb.net/test2?retryWrites=true&w=majority" || "mongodb://localhost/partyApp; 
+const dbPassword = "mongodb+srv://ravi:ravikumar@cluster0-nwcfy.mongodb.net/test2?retryWrites=true&w=majority" || "mongodb://localhost/partyApp"; 
 
 module.exports = {mongoURI: dbPassword};
